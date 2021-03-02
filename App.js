8e3f9d979b705bebae9efacfc15bb6b450cce711
brianbowen <bbowen1036@gmail.com>
2021-03-02 12:27:45 -0800

    custom Fonts

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 // React
-import React from "react";
+import React, { useState } from "react";
+
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font"
+
 // Redux
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from "redux";
@@ -15,7 +19,22 @@ const rootReducer = combineReducers({
 
 const store = createStore(rootReducer);
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  })
+}
+
 export default function App() {
+  const [ fontLoaded, setFontLoaded ] = useState(false);
+
+  if (!fontLoaded) {
+    return <AppLoading startAsync={fetchFonts} onFinish={() => {
+      setFontLoaded(true)
+    }} onError={() => {}} />
+  }
+
   return (
     <Provider store={store}>
       <ShopNavigator />
