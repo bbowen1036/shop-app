ac1dca79ccf3e9e7ebe36f377ecf04553b951d14
brianbowen <bbowen1036@gmail.com>
2021-03-06 20:55:15 -0800

    User input for edit screen

--- screens/user/EditProductScreen.js
+++ screens/user/EditProductScreen.js
@@ -1,15 +1,106 @@
-import React from "react";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, ScrollView, TextInput, Platform } from "react-native";
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
+// Redux
+import { useSelector } from "react-redux";
+// Compoents 
+import CustomHeaderButton from "../../components/UI/HeaderButton";
 
-import { View, Text, StyleSheet} from "react-native";
-const EditProductScreen = props => {
-  return(
-    <View>
-      <Text>The Edit Product Screen</Text>
-    </View>
-  )  
+
+const EditProductScreen = (props) => {
+  const prodId = props.navigation.getParam("productId");
+  // Find product first before initializing state to pre-populate fields
+  const editedProduct = useSelector((state) =>
+    state.products.userProducts.find((prod) => prod.id === prodId)
+  );
+
+  const [title, setTitle] = useState(editedProduct ? editedProduct.title : "");
+  const [imageUrl, setImageUrl] = useState(
+    editedProduct ? editedProduct.imageUrl : ""
+  );
+  const [price, setPrice] = useState("");                   // I am not allowing for price to be edited
+  const [description, setDescription] = useState(
+    editedProduct ? editedProduct.description : ""
+  ); /// bind to form fields to create controlled field
+
+  return (
+    <ScrollView>
+      <View style={styles.form}>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Title</Text>
+          <TextInput
+            style={styles.input}
+            value={title}
+            onChange={(text) => setTitle(text)}
+          />
+        </View>
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Image URL</Text>
+          <TextInput
+            style={styles.input}
+            value={imageUrl}
+            onChange={(imageUrl) => setImageUrl(imageUrl)}
+          />
+        </View>
+        {editedProduct ? null : (
+          <View style={styles.formControl}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput
+              style={styles.input}
+              value={price}
+              onChange={(text) => setPrice(text)}
+            />
+          </View>
+        )}
+        <View style={styles.formControl}>
+          <Text style={styles.label}>Description</Text>
+          <TextInput
+            style={styles.input}
+            value={description}
+            onChange={(text) => setDescription(text)}
+          />
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+
+EditProductScreen.navigationOptions = (navData) => {
+  return {
+    headerTitle: navData.navigation.getParam("productId")
+      ? "Edit Product"
+      : "Add Product", // Because we only pass additional params in edit mode
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item
+          title="Save"
+          iconName={Platform.OS === "android" ? "md-checkmark" : "ios-checkmark"}
+          onPress={() => {  // submit. will require useState
+            
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
+  form: {
+    margin: 20
+  },
+  formControl: {
+    width: "100%"
+  },
+  label: {
+    fontFamily: "open-sans-bold",
+    marginVertical: 8
+  },
+  input:{
+    paddingHorizontal: 2,
+    paddingVertical: 5,
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
 });
 
 export default EditProductScreen;
